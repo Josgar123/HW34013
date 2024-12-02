0774771b91e2069942c3edbb43129319f212d498
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-01 21:10:42 -0600

    Create view-viewers.php

---
+++ view-viewers.php
@@ -0,0 +1,26 @@
+<h1>Viewers</h1>
+<div class="table-responsive">
+  <table class="table">
+  <thead>
+    <tr>
+    <th>ID </th> 
+    <th>Name</th>
+    <th>Description</th> 
+    </tr>
+    
+  </thead> 
+    <tbody>
+      <?php
+while ($viewer = $viewers->fetch_assoc()) {
+  ?>
+  <tr>
+    <td> <?php echo $movie['viewer_id'];?> </td>
+    <td> <?php echo $movie['viewer_name'];?> </td>
+    <td> <?php echo $movie['viewer_description'];?> </td>
+  </tr>
+  <?php
+}
+     ?>
+    </tbody>
+  </table>
+</div>
