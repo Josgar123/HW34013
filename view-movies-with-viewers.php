a662834fbc75e97b0791886119c6a7f138f6ecfd
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-02 06:16:07 -0600

    Update view-movies-with-viewers.php

--- view-movies-with-viewers.php
+++ view-movies-with-viewers.php
@@ -8,13 +8,17 @@ while ($movie = $movies->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"> <?php echo $movie['movie_name'];?> </h5>
       <p class="card-text">
+      <ul class="list-group">
         <?php
   $viewers = selectViewersByMovies($movie['movie_id']);
   while ($viewer = $courses->fetch_assoc()) {
     ?>
+    <li class="list-group-item"><?php echo $movie['viewer_name'];?> - <?php echo $movie['month'];?> - <?php echo $movie['year'];?> - <?php echo $movie['day_time'];?></li>
+      
     <?php
     }
     ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Theater: <?php echo $movie['movie_theater'];?> </small></p>
     </div>
