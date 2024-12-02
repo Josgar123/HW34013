02fcb46aa2606ba4df1a2796d805f09e88c74574
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-01 21:25:33 -0600

    Create viewers-by-movie.php

---
+++ viewers-by-movie.php
@@ -0,0 +1,8 @@
+<?php
+require_once("db-util.php");
+require_once("model-viewers-by-movie.php");
+$pageTitle = "Viewers by Movie";
+include "view-header.php";
+$movies = selectViewersByMovie($_GET['id']);
+include "view-viewers-by-movie.php";
+include "view-footer.php";
