cab39e7c267e904bbe5efae04bf89853f435f8f1
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-01 21:59:37 -0600

    Create model-months-by-viewer.php

---
+++ model-months-by-viewer.php
@@ -0,0 +1,16 @@
+<?php
+function selectMonthsByViewer($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT v.viewer_id, viewer_name, viewer_description, month, year, day_time FROM 'viewer' v join section s on s.viewer.id = v.viewerid where s.viewer_id=1");
+        $stmt->bind_param("i",$cid); // look again to see if it matches
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
