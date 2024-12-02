c5f00d9579591b290b068258339f76b4798d429d
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-01 22:20:49 -0600

    Update view-viewers.php

--- view-viewers.php
+++ view-viewers.php
@@ -3,11 +3,11 @@
   <table class="table">
   <thead>
     <tr>
-    <th>ID </th> 
+    <th>ID</th> 
     <th>Name</th>
     <th>Description</th> 
+      <th> </th>
     </tr>
-    
   </thead> 
     <tbody>
       <?php
@@ -17,6 +17,12 @@ while ($viewer = $viewers->fetch_assoc()) {
     <td> <?php echo $movie['viewer_id'];?> </td>
     <td> <?php echo $movie['viewer_name'];?> </td>
     <td> <?php echo $movie['viewer_description'];?> </td>
+    <td>
+      <form method="post" action"months-by-viewer.php">
+        <input type="hidden" name="cid" value="<?php echo $movie['viewer_id'];?">
+  <button type="submit" class="btn btn-primary">Months</button>
+</form>
+    </td>
   </tr>
   <?php
 }
