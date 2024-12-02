8521a494efa9c1a923083183210213f96bec3b93
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-02 05:53:22 -0600

    Update model-movies.php

--- model-movies.php
+++ model-movies.php
@@ -12,4 +12,21 @@ function selectMovies() {
         throw $e;
     }
 }
+
+<?php
+function selectViewersByMovie() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT v.viewer_id, viewer_name, viewer_description, month, year, day_time FROM 'viewer' v join section s on s.viewer.id = v.viewerid where s.movie_id=?");
+        $stmt->bind_param("i",$iid); // look again to see if it matches
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
 ?>
