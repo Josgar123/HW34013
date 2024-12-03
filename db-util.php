0e1f2c0fe54a9693096a9a863e9fcdff24ce0d0c
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-03 13:51:48 -0600

    Update db-util.php

--- db-util.php
+++ db-util.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('', '', '', '');
+    $conn = new mysqli('mis4013db1.mysql.database.azure.com', 'josuegarcia', 'B34in123$', 'movie');
     
     // Check connection
     if ($conn->connect_error) {
