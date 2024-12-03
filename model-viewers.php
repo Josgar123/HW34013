ce2edd3581956a68e746e1cae42483f46c1bcc6b
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-03 09:58:56 -0600

    Update model-viewers.php

--- model-viewers.php
+++ model-viewers.php
@@ -12,4 +12,44 @@ function selectViewers() {
         throw $e;
     }
 }
+
+function insertViewer($vName, $vDesc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO 'viewer' ('viewer_name', 'viewer_description') VALUES (?,?)");
+        $stmt->bind_param("ss", $vName, $vDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateViewer($vName, $vDesc, $vid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE 'viewer' set 'viewer_name'=?, 'viewer_description'=? where course_id=?");
+        $stmt->bind_param("ssi", $vName, $vDesc, $vid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteViewer($vid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("Delete from course where course_id = ?");
+        $stmt->bind_param("i", $vid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
