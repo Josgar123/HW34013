372175ab0473c3ddcb45d25573f7607d327fb595
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-03 19:40:59 -0600

    Update model-viewers-by-movie.php

--- model-viewers-by-movie.php
+++ model-viewers-by-movie.php
@@ -2,7 +2,7 @@
 function selectViewersByMovie($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT v.viewer_id, viewer_name, viewer_description, month, year, day_time FROM 'viewer' v join section s on s.viewer.id = v.viewerid where s.movie_id=?");
+        $stmt = $conn->prepare("SELECT v.viewer_id, viewer_name, viewer_description FROM 'viewer' v join movie m on m.viewer.id = v.viewerid where m.movie_id=?");
         $stmt->bind_param("i",$iid); // look again to see if it matches
         $stmt->execute();
         $result = $stmt->get_result();
