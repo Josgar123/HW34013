e712483ff9af04a8955079a1b2f8795e9ec23285
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-04 13:28:19 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -27,7 +27,7 @@
           <a class="nav-link" href="movies.php">Movies</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="preferred-customer-movie.php">Preferred Customer Movie</a>
+          <a class="nav-link" href="preferred-customer-movie.php">Preferred Viewer Movie</a>
         </li>
       </ul>
     </div>
