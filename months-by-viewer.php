1411d7831ba1c32efab3ed712ce86fb381fd9280
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-04 12:40:23 -0600

    Update months-by-viewer.php

--- months-by-viewer.php
+++ months-by-viewer.php
@@ -1,6 +1,6 @@
 <?php
 require_once("db-util.php");
-require_once("model-sections-by-viewer.php");
+require_once("model-months-by-viewer.php");
 $pageTitle = "Movie by Directors";
 include "view-header.php";
 $months = selectMonthsByViewer($_POST['cid']);
