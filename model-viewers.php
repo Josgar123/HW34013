4914f1b6191422709565143f6480c6c26e3adf00
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-04 11:50:03 -0600

    Update model-viewers.php

--- model-viewers.php
+++ model-viewers.php
@@ -2,7 +2,7 @@
 function selectViewers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT viewers_id, viewers_name, viewers_description FROM 'viewer'");
+        $stmt = $conn->prepare("SELECT viewer_id, viewer_name, viewer_description FROM viewer");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
