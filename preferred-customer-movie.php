d71c0d4bd505f1d3dc7641d54d1a52b794107b02
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-04 13:27:58 -0600

    Update preferred-customer-movie.php

--- preferred-customer-movie.php
+++ preferred-customer-movie.php
@@ -1,7 +1,7 @@
 <?php
 require_once("db-util.php");
 require_once("model-prefered-customer-movie.php");
-$pageTitle = "Preferred-Customer-Movie";
+$pageTitle = "Preferred-Viewer-Movie";
 include "view-header.php";
 $movies = selectMovies();
 include "model-prefered-customer-movie.php";
