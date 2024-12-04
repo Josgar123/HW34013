62e96cd5113c9b07dea38298fd29fb05a8bfef77
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-03 19:30:41 -0600

    Update model-movies.php

--- model-movies.php
+++ model-movies.php
@@ -2,7 +2,7 @@
 function selectMovies() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT movie_id, movie_name, theater_name FROM 'movie'");
+        $stmt = $conn->prepare("SELECT movie_id, movie_name, runtime, year, genre FROM 'movie'");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -17,7 +17,7 @@ function selectMovies() {
 function selectViewersByMovie() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT v.viewer_id, viewer_name, viewer_description, month, year, day_time FROM 'viewer' v join section s on s.viewer.id = v.viewerid where s.movie_id=?");
+        $stmt = $conn->prepare("SELECT v.viewer_id, viewer_name, viewer_description FROM 'viewer' v join movie m on m.viewer.id = v.viewerid where m.movie_id=?");
         $stmt->bind_param("i",$iid); // look again to see if it matches
         $stmt->execute();
         $result = $stmt->get_result();
