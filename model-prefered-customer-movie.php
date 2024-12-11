a1e3e7e33123eff50d38ba813d296717b03cd204
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-11 09:10:13 -0600

    Update model-prefered-customer-movie.php

--- model-prefered-customer-movie.php
+++ model-prefered-customer-movie.php
@@ -2,7 +2,8 @@
 function selectMovies() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT movie_id, movie_name, runtime, year, genre FROM movie");
+        $stmt = $conn->prepare("SELECT m.movie_id, movie_name, runtime, year, genre, v.viewer_id, viewer_name, viewer_description FROM movie m 
+        join movieviewer mv on mv.movie_id=m.movie_id join viewer v on v.viewer_id=mv.viewer_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
