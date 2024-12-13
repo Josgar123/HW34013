64d6789d36f88d16e8bf6b8cfb6675dfb1a3d737
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 13:59:36 -0600

    Update viewers.php

--- viewers.php
+++ viewers.php
@@ -3,6 +3,15 @@ require_once("db-util.php");
 require_once("model-viewers.php");
 $pageTitle = "Viewers";
 include "view-header.php";
+
+if(isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+    insertViewer($_POST['vName'], $_POST['vDesc']);
+    break;
+  }
+}
+
 $viewers = selectViewers();
 include "view-viewers.php";
 include "view-footer.php";
