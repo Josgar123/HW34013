35bea529b5aa6ac0dcd7cf64e8249ce742130fd9
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 14:04:18 -0600

    Update viewers.php

--- viewers.php
+++ viewers.php
@@ -7,7 +7,11 @@ include "view-header.php";
 if(isset($_POST['actionType'])) {
   switch ($_POST['actionType']){
     case "Add":
-    insertViewer($_POST['vName'], $_POST['vDesc']);
+    if(insertViewer($_POST['vName'], $_POST['vDesc'])){
+    echo'<div class="alert alert-success" role="alert">Viewer Added!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
     break;
   }
 }
