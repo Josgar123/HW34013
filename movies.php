73b1fd763455ca2baa9d3b2e08d2477e1fd2247f
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 15:37:41 -0600

    Update movies.php

--- movies.php
+++ movies.php
@@ -3,6 +3,33 @@ require_once("db-util.php");
 require_once("model-movies.php");
 $pageTitle = "Movies";
 include "view-header.php";
+
+if(isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+    if(insertViewer($_POST['mName'], $_POST['mRuntime'], $_POST['mYear'], $_POST['mGenre'])){
+    echo'<div class="alert alert-success" role="alert">Movie Added!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
+    break;
+     case "Edit":
+    if(updateViewer($_POST['mName'], $_POST['mRuntime'], $_POST['mYear'], $_POST['mGenre'], $_POST['mid'])){
+    echo'<div class="alert alert-success" role="alert">Movie Edited!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
+    break;
+     case "Delete":
+    if(deleteViewer($_POST['mid'])){
+    echo'<div class="alert alert-success" role="alert">Movie Deleted!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
+    break;
+  }
+}
+
 $movies = selectMovies();
 include "view-movies.php";
 include "view-footer.php";
