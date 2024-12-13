3e08e7c1e5d1af816d9b2573ae31aa51b20815ca
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 09:06:25 -0600

    Update view-viewers.php

--- view-viewers.php
+++ view-viewers.php
@@ -19,7 +19,7 @@ while ($viewer = $viewers->fetch_assoc()) {
     <td> <?php echo $viewer['viewer_description'];?> </td>
     <td>
       <form method="post" action="months-by-viewer.php">
-        <input type="hidden" name="cid" value="<?php echo $viewer['viewer_id'];?>"
+        <input type="hidden" name="cid" value="<?php echo $viewer['viewer_id'];?>">
   <button type="submit" class="btn btn-primary"></button>
 </form>
     </td>
