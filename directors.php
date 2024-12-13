7a128a450bfc99b3b6a088f41279d271dc0bac95
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 16:54:01 -0600

    Update directors.php

--- directors.php
+++ directors.php
@@ -3,6 +3,33 @@ require_once("db-util.php");
 require_once("model-directors.php");
 $pageTitle = "Directors";
 include "view-header.php";
+
+if(isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+    if(insertDirector($_POST['dName'], $_POST['dAge'], $_POST['dMovies'])){
+    echo'<div class="alert alert-success" role="alert">Director Added!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
+    break;
+     case "Edit":
+    if(updateDirector($_POST['dName'], $_POST['dAge'], $_POST['dMovies'], $_POST['did'])){
+    echo'<div class="alert alert-success" role="alert">Director Edited!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
+    break;
+     case "Delete":
+    if(deleteDirector($_POST['did'])){
+    echo'<div class="alert alert-success" role="alert">Director Deleted!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
+    break;
+  }
+}
+
 $directors = selectDirectors();
 include "view-directors.php";
 include "view-footer.php";
