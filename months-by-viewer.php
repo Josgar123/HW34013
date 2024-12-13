e427ec7ec96b340ff7995de6069c04626f52cddf
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 17:32:54 -0600

    Update months-by-viewer.php

--- months-by-viewer.php
+++ months-by-viewer.php
@@ -3,6 +3,33 @@ require_once("db-util.php");
 require_once("model-months-by-viewer.php");
 $pageTitle = "Movie by Directors";
 include "view-header.php";
+
+if(isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+    if(insertMovieViewer($_POST['mid'], $_POST['vid'])){
+    echo'<div class="alert alert-success" role="alert">Movie Viewer Added!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
+    break;
+     case "Edit":
+    if(updateMovieViewer($_POST['mid'], $_POST['vid'], $_POST['mmid'])){
+    echo'<div class="alert alert-success" role="alert">Movie Viewer Edited!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
+    break;
+     case "Delete":
+    if(deleteMovieViewer($_POST['mmid'])){
+    echo'<div class="alert alert-success" role="alert">Movie Viewer Deleted!</div>';
+      } else {
+    echo '<div class="alert alert-danger" role="alert">Error!</div>';
+    }
+    break;
+  }
+}
+
 $movies = selectMonthsByViewer($_POST['cid']);
 include "view-months-by-viewers.php";
 include "view-footer.php";
