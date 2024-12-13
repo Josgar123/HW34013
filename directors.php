5f3b093257904c163d80429b2d8549b899286257
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-12 19:28:19 -0600

    Create directors.php

---
+++ directors.php
@@ -0,0 +1,9 @@
+<?php
+require_once("db-util.php");
+require_once(model-directors.php");
+$pageTitle = "Directors";
+include "view-header.php";
+$directors = selectDirectors();
+include "view-directors.php";
+include "view-footer.php";
+    ?>
