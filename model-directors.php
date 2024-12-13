c5de9e5c7b17189937c6aba338c9f244b31b9d4f
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 11:55:55 -0600

    Update model-directors.php

--- model-directors.php
+++ model-directors.php
@@ -12,4 +12,46 @@ function selectDirectors() {
         throw $e;
     }
 }
+function insertDirector($dName, $dAge, $dMovies) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("insert into director (director_name, director_age, movies_made) values (?,?,?)");
+        $stmt->bind_param("sis", $dName, $dAge, $dMovies);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateDirector($dName, $dAge, $dMovies, $did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update director set director_name=?, director_age=?, movies_made=? where movie_id=?");
+        $stmt->bind_param("sisi", $dName, $dAge, $dMovies);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteDirector($did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from director where director_id=?");
+        $stmt->bind_param("i", $did);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
