8c375bd0a791e569b9975614d52caa497338d8d2
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 21:57:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<div style="background-color: beige; padding: 300px;">
+<div style="background-image: url('https://wallpapers.com/images/featured/movie-9pvmdtvz4cb0xl37.jpg'); background-size: cover; background-position: center; padding: 300px; color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);">
     <h1 style="text-align: center;">Movie Magic & Viewer Chronicles</h1>
-<img src="https://wallpapers.com/images/featured/movie-9pvmdtvz4cb0xl37.jpg"" class="img-fluid">
-   <?php
+</div>
+<?php
 include "view-footer.php";
-    ?>
+?>
