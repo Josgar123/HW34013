35ec3b9129cfef70367ea9d50ef824c72817b728
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 18:33:44 -0600

    Create view-movie-input-list.php

---
+++ view-movie-input-list.php
@@ -0,0 +1,9 @@
+<select class="form-select" id="mid" name="mid">
+<?php
+while ($movieItem = $movieList-> fetch_assoc()) {
+  ?>
+   <option value="<?php echo movieItem['movie_id'];"><?php echo movieItem['movie_name'];</option>
+  <?php
+}
+?>
+</select>
