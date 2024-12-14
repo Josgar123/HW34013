85476e2db2e14f480f30734ab521b83ce9666c56
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 21:56:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 <div style="background-color: beige; padding: 300px;">
-    <h1 style="text-align: center;">Movies</h1>
+    <h1 style="text-align: center;">Movie Magic & Viewer Chronicles</h1>
 <img src="https://wallpapers.com/images/featured/movie-9pvmdtvz4cb0xl37.jpg"" class="img-fluid">
    <?php
 include "view-footer.php";
