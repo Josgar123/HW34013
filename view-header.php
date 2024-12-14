58b0a2b1430d2982fcbc112b04551e629c690e08
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 22:03:53 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?> </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-   <style>
+   
    <style>
   body {
     background-image: url('https://wallpapers.com/images/featured/movie-9pvmdtvz4cb0xl37.jpg');
@@ -21,7 +21,6 @@
   }
 </style>
 
-   </style>
   </head>
   <body style="background-color: beige;">
   <div class="container">
