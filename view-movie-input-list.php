5d86097b9726c82fcbceeb74a3ea39e99eb8ff15
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 18:49:10 -0600

    Update view-movie-input-list.php

--- view-movie-input-list.php
+++ view-movie-input-list.php
@@ -2,7 +2,7 @@
 <?php
 while ($movieItem = $movieList-> fetch_assoc()) {
   ?>
-   <option value="<?php echo movieItem['movie_id'];"><?php echo movieItem['movie_name'];</option>
+   <option value="<?php echo movieItem['movie_id'];?>"><?php echo movieItem['movie_name'];?> </option>
   <?php
 }
 ?>
