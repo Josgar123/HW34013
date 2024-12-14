5edf2e4bba6f0228b3e6de7f8c283cde34790b4e
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 22:28:34 -0600

    Update model-months-by-viewer.php

--- model-months-by-viewer.php
+++ model-months-by-viewer.php
@@ -5,9 +5,10 @@ function selectMonthsByViewer($cid) {
         $stmt = $conn->prepare("SELECT m.movie_id, movie_name, runtime, v.viewer_id, viewer_name, viewer_description FROM movie m 
         join movieviewer mv on mv.movie_id=m.movie_id join viewer v on v.viewer_id=mv.viewer_id where m.movie_id=?");
         $stmt->bind_param("i",$cid); // look again to see if it matches
-        $success=$stmt->execute();
+        $stmt->execute();
+        $result = $stmt->get_result();
         $conn->close();
-        return $success;
+        return $result;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
