665f1f0853e4360054eb682be5250b36784f2c25
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 21:53:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include "view-header.php";
 ?>
 <div style="background-color: beige; padding: 300px;">
     <h1 style="text-align: center;">Movies</h1>
+<img src="https://wallpapers.com/images/featured/movie-9pvmdtvz4cb0xl37.jpg"" class="img-fluid">
    <?php
 include "view-footer.php";
     ?>
