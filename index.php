07677c556edbf590c2e2d63431c17e01609958ec
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-13 20:31:49 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<div style="background-color: beige; padding: 100px;">
+<div style="background-color: beige; padding: 400px;">
     <h1 style="text-align: center;">Movies</h1>
    <?php
 include "view-footer.php";
